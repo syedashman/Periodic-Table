7251db8f50e6cce7ed739c7830a73c88933f32b7
Syed Muhammad Ashman <107072014+syedashman@users.noreply.github.com>
2024-08-03 16:20:29 +0500

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,37 @@
+// Operators in java script
+// 1) Unary Operators
+// 2) Binary Operators
+
+
+// Operand: entities on which operators operate.
+// e.g: 3 + 4 = 7
+// here '3 & 4' are operators while, '+' is the operand.
+// Unary Operators: It has single operand (x = -x)
+// For Example,
+a = 29.65;
+a = -a
+console.log(a);
+
+// Binary Operators: It has single operand (x = x+8)
+// For Example,
+a= 67+2; 
+console.log(a)
+
+// Arithmetic Operators
+let num1 = 56;
+let num2 = 13;
+console.log("The value of num1 + num2 is " + (num1 + num2));
+console.log("The value of num1 - num2 is " + (num1 - num2));
+console.log("The value of num1 * num2 is " + (num1 * num2));
+console.log("The value of num1 / num2 is " + (num1 / num2));
+
+// Increment Operators
+let val1 = 2;
+console.log("The value of val1 ++ is " + (val1 ++));
+console.log("The value of ++ val1 is " + (++ val1 ));
+
+
+// Decrement Operators
+let val2 = 4;
+console.log("The value of val2 -- is " + (val2 --));
+console.log("The value of -- val2 is " + (-- val2 ));
