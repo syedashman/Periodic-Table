172cf27f9a8605876152dd70c20dc1a3b11902dd
Syed Muhammad Ashman <107072014+syedashman@users.noreply.github.com>
2024-08-03 16:20:04 +0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,9 @@
+var name = "Syed Muhammad Ashman"
+var email = "syedashmanmumtaz@gmail.com";
+var message = "Ashman is a good boy";
+var temp = ` This guy name is "${name}" and he has an email that is "${email}"`;
+var len = name.length;
+console.log(`Length of name is ${len}`)
+console.log(temp)
+console.log("Hello World\nAshman\tHow are\byou")
+
